ea7ca5c6fa2338c2c2b8003087eb06370c6aac12
calonbupati <calonbupatii@gmail.com>
2020-08-03 12:36:00 +0700

    Revert "Update index.js root route response to Hello World"
    This reverts commit aac48485bf7e74ef7697d95b431485e121fff46c.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-	res.send('Hello World')
+	res.send('Test')
 })
 
 app.get('/about', function (req, res) {
